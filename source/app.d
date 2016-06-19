75ba27e70b4e7b74acc7e8d393e3594a82ac6f29
Yannick Koechlin <yannick@koechlin.info>
2016-06-19 14:45:53 +0200

    formatting

--- source/app.d
+++ source/app.d
@@ -16,24 +16,24 @@ import std.string;
 
 struct Item
 {
-	string name;
-	string description;
-	string html_url;
+    string name;
+    string description;
+    string html_url;
 }
 
 void main()
 {
 
-	string endpoint = "https://api.github.com/search/repositories?order=desc&sort=updated&q=language:D";
-	
-	auto rq = Request();
+    string endpoint = "https://api.github.com/search/repositories?order=desc&sort=updated&q=language:D";
+
+    auto rq = Request();
     rq.useStreaming = true;
     rq.verbosity = 2;
     auto rs = rq.get(endpoint);
     auto stream = rs.receiveAsRange();
 
-	auto items = stream.parseJson["items"].deserialize!(Item[]);
-	
-	items.each!(i => writefln("%s\t%s\t%s", i.name, i.description, i.html_url) );
+    auto items = stream.parseJson["items"].deserialize!(Item[]);
+
+    items.each!(i => writefln("%s\t%s\t%s", i.name, i.description, i.html_url));
 
 }
