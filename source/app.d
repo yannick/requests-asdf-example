0626823f17bc5d1d5f4396736a87482198094f79
Yannick Koechlin <yannick@koechlin.info>
2016-06-19 14:44:57 +0200

    use requests streaming mode

--- source/app.d
+++ source/app.d
@@ -23,20 +23,17 @@ struct Item
 
 void main()
 {
-	//last recently updated D projects on github
+
 	string endpoint = "https://api.github.com/search/repositories?order=desc&sort=updated&q=language:D";
 	
+	auto rq = Request();
+    rq.useStreaming = true;
+    rq.verbosity = 2;
+    auto rs = rq.get(endpoint);
+    auto stream = rs.receiveAsRange();
 
-	auto items = getContent(endpoint).to!string.parseJson["items"].deserialize!(Item[]);
+	auto items = stream.parseJson["items"].deserialize!(Item[]);
 	
 	items.each!(i => writefln("%s\t%s\t%s", i.name, i.description, i.html_url) );
-
-	////version 2
-	//auto r = new Request();
-	//auto result = r.get(endpoint).responseBody.data!string.deserialize!Result;
-	//foreach(i; result.items)
-	//{
-	//	writefln("%s\t%s\t%s", i.name, i.description, i.html_url);
-	//}
 
 }
